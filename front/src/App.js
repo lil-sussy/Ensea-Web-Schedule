51d4a01661ff32121288dcff16b2a41f4c1a8bd9
naykeys <naykeysnet@gmail.com>
2022-10-20 22:36:10 +0100

    UWU

--- front/src/App.js
+++ front/src/App.js
@@ -1,31 +1,32 @@
-import React, { Component }  from 'react';
 import './App.css';
 import WeekSchedule from "./DayCarousel";
 
 function App() {
-  const weekData = [ {day: 'monday', courses: [ { name: 'Syslin', begin: '8h', end: '10h', } ]},
-      { day: 'tuesday', courses: [ {name: 'Syslin', begin: '8h', end: '10h', teachers: 'M. Jebri', classes: '1G1 TD3', place: 'A211'}, {name: 'Syselec', begin: '10h', end: '12h', teachers: 'M. Jebri', classes: '1G1 TD3', place: 'A211'}, {name: 'Syslin', begin: '13h30', end: '15h30', teachers: 'M. Jebri', classes: '1G1 TD3', place: 'A211'}, {name: 'Syslin', begin: '15h30', end: '17h30', teachers: 'M. Jebri', classes: '1G1 TD3', place: 'A211'}, {name: 'Syslin', begin: '17h30', end: '20h', teachers: 'M. Jebri', classes: '1G1 TD3', place: 'A211'} ]},
-      { day: 'wednesday', courses: [ {name: 'Syslin', begin: '8h', end: '10h', teachers: 'M. Jebri', classes: '1G1 TD3', place: 'A211'} ]},
-      { day: 'thursday', courses: [ {name: 'Syslin', begin: '8h', end: '10h', teachers: 'M. Jebri', classes: '1G1 TD3', place: 'A211'} ]},
-      { day: 'friday', courses: [ {name: 'Syslin', begin: '8h', end: '10h', teachers: 'M. Jebri', classes: '1G1 TD3', place: 'A211'} ]},
+  const weekData = [ { date: '16/10/2022', day: 'lundi', courses: [ { name: 'Systèmes Linéaires', begin: '8h', end: '10h', teachers: 'M.Jebri', classes: '1G1 TD3', place: 'A211' } ]},
+      { date: '17/10/2022', day: 'mardi', courses: [ {name: 'Systèmes Linéaires', begin: '8h', end: '10h', teachers: 'M. Jebri', classes: '1G1 TD3', place: 'A211'}, {name: 'Systèmes électroniques', begin: '10h', end: '12h', teachers: 'M. Duperrier', classes: '1G1 TD3', place: 'A211'}, {name: 'Systèmes Linéaires', begin: '13h30', end: '15h30', teachers: 'M. Jebri', classes: '1G1 TD3', place: 'A211'}, {name: 'Systèmes Linéaires', begin: '15h30', end: '17h30', teachers: 'M. Jebri', classes: '1G1 TD3', place: 'A211'}, {name: 'Systèmes Linéaires', begin: '17h30', end: '20h', teachers: 'M. Jebri', classes: '1G1 TD3', place: 'A211'} ]},
+      { date: '18/10/2022', day: 'mercredi', courses: [ {name: 'Systèmes Linéaires', begin: '8h', end: '10h', teachers: 'M. Jebri', classes: '1G1 TD3', place: 'A211'} ]},
+      { date: '19/10/2022', day: 'jeudi', courses: [ {name: 'Systèmes Linéaires', begin: '8h', end: '10h', teachers: 'M. Jebri', classes: '1G1 TD3', place: 'A211'} ]},
+      { date: '20/10/2022',  day: 'vendredi', courses: [ {name: 'Systèmes Linéaires', begin: '8h', end: '10h', teachers: 'M. Jebri', classes: '1G1 TD3', place: 'A211'} ]},
   ];
   return (
     <div className="App">
-      <div className="background blur-small bg-cover bg-center h-screen w-screen"></div>
-      <header className=" w-full h-full App-header -translate-x-1/2 absolute left-1/2 top-0 flex-col items-center justify-top">
-        <img className="m-2 w-12 h-12 rounded-full bg-black" alt=""></img>
-        <div id="sliders" className=" font backdrop-blur-small w-3/4 h-12 flex-col align-center justify-center rounded-xl">
-          <div className=" w-full h-full bg-white rounded-xl opacity-75">Syslin
-            <div className=" mx-2 h-1/2 text-gray-800 justify-center align-center text-center">
+      <div className="ENSEABackground background bg-cover bg-center h-screen w-screen transition"></div>
+      <header className="AppContainer text-zinc-800 w-full h-full App-header -translate-x-1/2 absolute left-1/2 top-0">
+        <div className="Logo rounded-full mx-auto my-4 w-20 h-20 shadow-xl" alt=""></div>
+        <div className="SelectionsContainer mx-auto font backdrop-blur-sm w-3/4 h-20 flex-col align-center justify-center rounded-xl">
+          <div className="Selections text-2xl w-full h-full bg-white rounded-xl opacity-75">
+            <div className="ClasseSelection h-1/2 justify-center align-center text-center">
               <h4>1G1 TD3</h4>
             </div>
             <hr></hr>
-            <div className=" mx-2 h-1/2 text-gray-800 justify-center align-center text-center">
+            <div className="WeekSelection h-1/2 justify-center align-center text-center">
               <h4>Semaine 9</h4>
             </div>
           </div>
         </div>
-        <WeekSchedule weekData={weekData}/>
+        <div className="WeekScheduleContainer my-1/20 w-full h-3/4">
+          <WeekSchedule weekData={weekData}/>
+        </div>
       </header>
     </div>
   );
--- front/src/DayCarousel.js
+++ front/src/DayCarousel.js
@@ -1,61 +1,64 @@
-import React, { Component } from 'react';
-
-import { Swiper, SwiperSlide } from "swiper/react";
+import { useState, useEffect } from "react";
+import { Swiper, SwiperSlide, SwiperProps } from "swiper/react";
 import "swiper/swiper-bundle.min.css";
 
-import SwiperCore, { EffectFlip, Navigation, Pagination } from "swiper";
+import SwiperCore, { EffectCreative, EffectFlip, EffectCube, Navigation, Pagination } from "swiper";
 
-
-const styles = {
-  focused: {
-    
-  },
-  sided: {
-    transform: 'scale(80%) translateX(0)',
-  }
-}
+import "swiper/css/effect-creative"
+SwiperCore.use([EffectFlip])
 
 let createDay = (dayData, focused) => {
   const courses = dayData.courses;
   const divCourses = [];
+  const colors = ['red', 'orange,', 'amber', 'yellow', 'lime', 'green', 'emerald', 'teal', 'cyan', 'sky', 'blue', 'indigo', 'violet', 'purple', 'fuchsia', 'pink', 'rose' ]
   courses.map((course) => {
-    let courseClasses = " w-3/4 h-full w-full bg-white rounded-xl opacity-85 ";
-    courseClasses += "begin-" + course.begin + " end-" + course.end + "";
+    let courseClasses = "Course h-full w-80 rounded-xl opacity-80";
+    const color = colors[Math.round(Math.random()*colors.length)]
+    courseClasses += " bg-"+color+"-100 begin-" + course.begin + " end-" + course.end + "";
     divCourses.push(
       <div key={course.begin} className={courseClasses}>
-        <h3> {course.name} </h3>
-        <h4> {course.teachers} </h4>
-        <h4> {course.classes} </h4>
-        <h4> {course.place} </h4>
+        <h3 className=""> {course.name} </h3>
+        <h4 className="text-sm"> {course.teachers} </h4>
+        <h4 className="text-sm"> {course.classes} </h4>
+        <h4 className="text-sm"> {course.place} </h4>
       </div>
     );
   });
   return (
-    <SwiperSlide key={dayData.day} className="">
-      <div className="">
-        <div className="absolute left-0 backdrop-blur-sm w-64 h-full rounded-3xl"
-          style={ focused ? styles.focused : styles.sided }>
-          <div className=" rounded-3xl w-full h-full bg-white opacity-70">
+    <SwiperSlide key={dayData.day} className={dayData.day}>
+      <div className="DayContainer w-full h-full text-lg flex justify-center items-center">
+        <div className="DayAbsoluteContainer absolute left--1/4 top-0 backdrop-blur-sm w-96 h-full rounded-3xl">
+          <div className="DayBackground rounded-3xl w-full h-full bg-white bg- opacity-70">
           </div>
-          <div className="flex flex-col justify-center items-start">
-            <div className=" w-3">
-              <h4 key="8h">8h</h4>
-              <h4 key="9h">9h</h4>
-              <h4 key="10h">10h</h4>
-              <h4 key="11h">11h</h4>
-              <h4 key="12h">12h</h4>
-              <h4 key="13h">13h</h4>
-              <h4 key="14h">14h</h4>
-              <h4 key="15h">15h</h4>
-              <h4 key="16h">16h</h4>
-              <h4 key="17h">17h</h4>
-              <h4 key="18h">18h</h4>
-              <h4 key="19h">19h</h4>
-              <h4 key="20h">20h</h4>
+          <div className="DayContent h-9/10 absolute top-2 flex justify-start">
+            <div className="Hours inline-grid w-12 h-full">
+              <h4 key="8h" className="Hour">8h</h4>
+              <h4 key="9h" className="Hour">9h</h4>
+              <h4 key="10h" className="Hour">10h</h4>
+              <h4 key="11h" className="Hour">11h</h4>
+              <h4 key="12h" className="Hour">12h</h4>
+              <h4 key="13h" className="Hour">13h</h4>
+              <h4 key="14h" className="Hour">14h</h4>
+              <h4 key="15h" className="Hour">15h</h4>
+              <h4 key="16h" className="Hour">16h</h4>
+              <h4 key="17h" className="Hour">17h</h4>
+              <h4 key="18h" className="Hour">18h</h4>
+              <h4 key="19h" className="Hour">19h</h4>
+              <h4 key="20h" className="Hour">20h</h4>
             </div>
-            {divCourses.map((div) => {
-              return div;
-            })}
+            <div className="CoursesContainer inline-grid grid-rows-40">
+              {divCourses.map((div) => {
+                return div;
+              })}
+            </div>
+          </div>
+          <div className="DayName absolute flex justify-center items-center flex-col text-center w-full bottom-0 h-1/7">
+            <h2 className = "text-4xl">
+              {dayData.day.toUpperCase()}
+            </h2>
+            <h4 className = "text-sm">
+              {dayData.date}
+            </h4>
           </div>
         </div>
       </div>
@@ -63,37 +66,73 @@ let createDay = (dayData, focused) => {
   );
 }
 
+
 let WeekSchedule = (props) => {
+  useEffect(() => {
+  }, [])
   const daysList = [];
   props.weekData.map((dayData) => {
     if (dayData.day !== "tuesday")
       daysList.push(createDay(dayData))
-    else 
+    else
       daysList.push(createDay(dayData, true))
   });
-  return (
-    <Swiper
-      className="msySwiper w-full h-full"
-      enabled={true}
-      direction="horizontal"
-      spaceBetween={100}
-      centeredSlides={true}
-      cssMode={true}
-      modules={[Pagination]}
-      slidesPerView={3}
-      speed={500}
-      touchRatio={1.5}
-      navigation={true}
-      loop={false}
-      effect={"slide"}  // slide fade cube coverflow flip creative
-      autoplay={false}
-      pagination={{ clickable: true }}
-    >
-      {daysList.map(day => {
-        return day;
-      })}
-    </Swiper>
-  );
+  const NiceAnimation = (swiper) => {
+    if (swiper.destroyed === true) {
+      return
+    }
+    const slideSize = swiper.size
+    const snapGrid = swiper.snapGrid
+    const swiperPos = -swiper.translate
+    const maxIndex = swiper.slides.length
+    for (let index = 0; index < maxIndex; index++) {
+      let p = - (snapGrid[index] - swiperPos) / slideSize
+      p = p < -1 ? -1 : p;
+      p = p > 1 ? 1 : p;
+      swiper.slides[index].style.transitionProperty = "transform"
+      swiper.slides[index].style.transitionDuration = "0ms"
+      swiper.slides[index].style.transform = "scale(" + (0.8 + 0.2*(1-Math.abs(p))) + ") translateX(" + (p * 40) + "%)"
+      swiper.slides[index].style.transitionProperty = "transform"
+      swiper.slides[index].style.transitionDuration = "700ms"
+    }
+  }
+  const onInit = (swiper) => {
+    NiceAnimation(swiper)
+    const observer = new MutationObserver(function (mutations) {
+      NiceAnimation(swiper)
+    });
+    const target = document.getElementsByClassName('swiper-wrapper');
+    observer.observe(target[0], { attributes: true, attributeFilter: ['style'] });
+  }
+  const onTransitionStart = (swiper) => {
+    for (let index = 0; index < swiper.slides.length; index++) {
+      swiper.slides[index].style.transitionProperty = "transform"
+      swiper.slides[index].style.transitionDuration = "1000ms"
+    }
+  }
+  const swiper = (<Swiper
+    className="WeekSchedule w-full h-full mb-6"
+    enabled={true}
+    direction="horizontal"
+    spaceBetween={0}
+    centeredSlides={true}
+    cssMode={false}
+    modules={[Pagination]}
+    slidesPerView={1}
+    speed={1000}
+    touchRatio={1.5}
+    navigation={false}
+    loop={false}
+    autoplay={false}
+    onTouchEnd={onTransitionStart}
+    onInit={onInit}
+    pagination={{ clickable: true }}
+  >
+    {daysList.map(day => {
+      return day;
+    })}
+  </Swiper>)
+  return swiper;
 }
 
 export default WeekSchedule;
