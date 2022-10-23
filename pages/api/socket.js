883f4b65a4a6a611c535428deac206f57e59f60f
NayKeys <naykeysnet@gmail.com>
2022-10-23 17:06:00 +0100

    socket io fix

--- pages/api/socket.js
+++ pages/api/socket.js
@@ -7,12 +7,15 @@ const SocketHandler = (req, res) => {
   } else {
     console.log('Socket is initializing')
     const io = new Server(res.socket.server, {
-      pingTimeout: 30000,
+      // pingTimeout: 30000,
       secure: true,
     })
     res.socket.server.io = io
     io.on('packet', (packet) => {
       console.log('received packet:', packet)
+    })
+    io.on('connect_error', (err) => {
+      console.log(err)
     })
     io.on('connection', function(socket) {
     
