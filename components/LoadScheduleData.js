fb5829f7e9afb12082ff6e7102fc6f36dc16237d
NayKeys <46908682+NayKeys@users.noreply.github.com>
2022-10-25 20:26:00 +0200

    Small Script fix

--- components/LoadScheduleData.js
+++ components/LoadScheduleData.js
@@ -36,71 +36,66 @@ function removeSpaces(str) {
   return str.slice(begin, str.length - end)
 }
 
+function hasNumber(myString) {
+  return /\d/.test(myString);
+}
+
+function saveCourse(dayID, weekID, data) {
+  const coursesIDs = [];
+  const courseData = [];
+  let dataBeginIndex = 0;
+  while(hasNumber(data[dataBeginIndex])) {
+    coursesIDs.push(removeSpaces(data[dataBeginIndex]))
+    dataBeginIndex++;
+  }
+  for (let dataIndex = 0; dataIndex < data.length - 1; dataIndex++) {  // Courses
+    if (data.length > 0) {
+      courseData.push(data[dataIndex])
+    }
+  }
+  const week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+  const hours = datat[data.length - 1].split(' - ')  // Last element : 8h00 - 12h00
+  const name = courseData[0]
+  
+  coursesIDs.forEach((courseID) => {
+    const docRef = doc(dbInstance, SCHEDULES, courseID, "week " + (weekID + 1), week[dayID], name)
+    const course = {
+      courseName: name,
+      courseID: courseID,
+      begin: hours[0],
+      end: hours[1],
+      courseData: courseData,
+    }
+    console.log("adding to %s this course %s", docRef, course)
+    setDoc(docRef, course)
+  })
+}
+
 export function loadScheduleDataFromString(data) {
   const headlength = "2['uwu-ade-weekly-shcedule// ".length // Replace char ' with char "
   const taillength = "']".length // removing the bracket at the end
   data = data.slice(headlength, data.length - taillength)
   const weeksData = data.split('/-/')
-  const week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
   let weekID; let dayID; let courseID;
-  for (let parseWeekIndex = 0; i < weeksData.length; i++) {  // Weeks
-    if (parseWeekIndex % 2 === 0) {
+  for (let parseWeekIndex = 0; parseWeekIndex < weeksData.length; parseWeekIndex++) {  // Weeks
+    if (parseWeekIndex % 2 === 1) {  // First element is "" the second is the weekID third is data and so on
       weekID = Number(weeksData[parseWeekIndex])
     } else {
       const daysData = weeksData[parseWeekIndex].split(';-;')
-      const courseIDs = new Map();
-      for (let parseDayIndex = 0; i < daysData.length; i++) {  // Days
-        if (parseDayIndex % 2 === 0) {
+      for (let parseDayIndex = 0; parseDayIndex < daysData.length; parseDayIndex++) {  // Days
+        if (parseDayIndex % 2 === 1) {
           dayID = Number(daysData[parseDayIndex])
         } else {
           const coursesData = daysData[parseDayIndex].split(';;')
-          const dayData = [];
-          for (let parseCourseIndex = 0; i < coursesData.length; i++) {  // Courses
-            if (parseDayIndex % 2 === 0) {
-              courseID = removeSpaces(coursesData[parseCourseIndex])
+          for (let parseCourseIndex = 0; parseCourseIndex < coursesData.length; parseCourseIndex++) {  // Courses
+            if (parseCourseIndex % 2 === 1) {
+              courseID = removeSpaces(coursesData[parseCourseIndex])  // Unused
             } else {
               const data = coursesData[parseCourseIndex].split(',,')
-              const courseData = [];
-              for (let dataIndex = 0; i < data.length-1; i++) {  // Courses
-                if (data.length > 0) {
-                  courseData.push(data[dataIndex])
-                }
-              }
-              const hours = datat[data.length -1].split(' - ')  // Last element : 8h00 - 12h00
-              courseIndex = courseIDs.get(courseID) === undefined ? 0 : courseIDs.get(courseID)
-              courseIndex += 1;
-              courseIDs.set(courseID, courseIndex)
-              const docRef = doc(dbInstance, SCHEDULES, courseID, weekID+1, dayID, courseIndex)
-              const course = {
-                courseID: courseID,
-                begin: hours[0],
-                end: hours[1],
-                courseData: courseData,
-              }
-              console.log("adding to %s this course %s", docRef, course)
-              setDoc(docRef, course)
+              saveCourse(dayID, weekID, data)
             }
           }
-          const day = {
-            name: week[parseDayIndex],
-            courses: dayData,
-          }
         }
-      }
-    }
-  }
-  for (let count = 0; count < daysData.length; count++) {
-    if (!Number.isNaN(Number(daysData[count]))) { // Then it's the dayIndex
-      currentDay = week[Number(daysData[count])]
-    } else {
-      const courses = data.split(';;') // ;; is the course splitter
-      for (let i = 0; i < courses.length; i++) {
-        const courseDataSplitted = courses[i].split(',,') // ,, is the information splitter
-        let informations = "" // Other informations of the course such as concerned clases and teachers name
-        for (let j = 1; j < courseDataSplitted.length - 1; j++) { informations += courseDataSplitted[j] }
-        let course = { name: courseDataSplitted[0], time: courseDataSplitted[courseDataSplitted.length - 1], informations: informations }
-        console.log(course)
-        schedule.push(currentDay, course) // adding course data to schedule
       }
     }
   }
