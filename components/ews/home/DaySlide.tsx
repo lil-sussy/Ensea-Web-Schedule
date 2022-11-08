251d115aec9e5b910453b794197fac63461f4fc3
NayKeys <naykeysnet@gmail.com>
2022-11-08 19:00:39 +0100

    Ui update seems over for now

--- components/ews/home/DaySlide.tsx
+++ components/ews/home/DaySlide.tsx
@@ -1,18 +1,22 @@
 import clsx from 'clsx';
-import { useState, useEffect, useMemo, Attributes, Key } from 'react';
+import { useState, useEffect, useMemo, Attributes, Key, ReactElement } from 'react';
 import { Swiper, SwiperSlide, SwiperProps } from "swiper/react";
 import "swiper/swiper-bundle.min.css";
 
 export default function DaySlide({ actualDay, date, dayData, loading }) {
   const courses = dayData;
-  const courseHourWrappers = [];
+  const courseHourWrappers = new Map()
   const divCourses = [];
+  const courseHourWrapperList = []
   if (courses != undefined) {  // It may be possible if a day is empty
     for (let i = 0; i < courses.length; i++) {
       const courseData = courses[i]
       divCourses.push(<Course courseData={courseData} />);
-      courseHourWrappers.push(CourseHours({ courseData: courseData, ending:false }));
+      courseHourWrappers.set(courseData.begin, CourseHours({ courseData: courseData, ending:false }));
+      if (!courseHourWrappers.get(courseData.end))  // We only display the end hour of the course if there is another course that starts after
+        courseHourWrappers.set(courseData.end, CourseHours({ courseData: courseData, ending:true }));
     }
+    courseHourWrappers.forEach((value, key) => courseHourWrapperList.push(value))
   } else {
     divCourses.push(
       <div className=''>
@@ -21,7 +25,7 @@ export default function DaySlide({ actualDay, date, dayData, loading }) {
   return (
     <SwiperSlide key={actualDay as Key}>
       <DayContainer>
-        <DayContent courses={divCourses} coursesHours={courseHourWrappers} />
+        <DayContent courses={divCourses} coursesHours={courseHourWrapperList} />
         <DayName name={actualDay} date={date} />
       </DayContainer>
     </SwiperSlide >
@@ -31,14 +35,11 @@ export default function DaySlide({ actualDay, date, dayData, loading }) {
 function CourseHours({ courseData: course, ending }) {
   const beginHour = Number(course.begin.slice(0, 2))
   const endHour = Number(course.end.slice(0, 2))
-  const wrpBeginHour = beginHour   // Wrapper of half an hour long
-  const wrpEndHour = endHour // Wrapping above the previous course
-  const wrpRowBegin = (wrpBeginHour - 6) * 2 + (course.begin.slice(3, 5) == '30' ? 1 : 0)   // Half hours are 1 row in length and the day starts at 6am
-  const wrpRowEnd = (wrpEndHour - 6) * 2 + (course.end.slice(3, 5) == '30' ? 1 : 0)   // Half hours are 1 row in length and the day starts at 7am
-  const endHourPos = (wrpEndHour - 0.5 - 6) * 2 + (course.end.slice(3, 5) == '30' ? 1 : 0)   // Half hours are 1 row in length and the day starts at 7am
+  const wrpRowBegin = (beginHour - 7) * 2 + (course.begin.slice(3, 5) == '30' ? 1 : 0)   // Half hours are 1 row in length and the day starts at 7am
+  const wrpRowEnd = (endHour - 7) * 2 + (course.end.slice(3, 5) == '30' ? 1 : 0)   // Half hours are 1 row in length and the day starts at 7am
   if (!ending)
     return (
-      <div key={wrpBeginHour} className={clsx(" w-full h- row-start-" + wrpRowBegin + " col-span-1 text-white font-bold relative")}
+      <div key={beginHour} className={clsx(" w-full h- row-start-" + wrpRowBegin + " col-span-1 font-bold relative")}
         style={{ gridRowStart: wrpRowBegin }}>
         <div className="w-12 ml-1 bg-third-purple rounded-md text-main-purple-light h-full flex flex-col z-20">
           <h3 className="text-center items-center text-[0.65rem] leading-[0.7rem] my-auto z-20">
@@ -50,14 +51,12 @@ function CourseHours({ courseData: course, ending }) {
     )
   else
     return (
-      <div key={wrpBeginHour} className={clsx(" w-full h- row-start-" + wrpRowBegin + " col-span-1 text-white font-bold relative")}
-        style={{ gridRowStart: wrpRowBegin }}>
-        <div className="w-12 ml-1 bg-third-purple rounded-md text-main-purple-light h-full flex flex-col z-20">
-          <h3 className="text-center items-center text-[0.65rem] leading-[0.7rem] my-auto z-20">
-            {course.begin}
-          </h3>
-        </div>
-        <div className="w-full absolute top-1/2 h-[0.15rem] bg-gradient-to-r from-main-purple-light to-main-purple -z-10"></div>
+      <div key={endHour} className={clsx(" w-full h- row-start-" + wrpRowEnd + " col-span-1 font-bold relative \
+      w-12 ml-1 rounded-md text-main-purple-light h-full flex flex-col ")}
+      style={{ gridRowStart: wrpRowEnd }}>
+        <h3 className="text-center items-center text-[0.65rem] leading-[0.7rem] my-auto z-20">
+          {course.end}
+        </h3>
       </div>
     )
 }
@@ -65,22 +64,22 @@ function CourseHours({ courseData: course, ending }) {
 function Course({ courseData: course }) {
   const beginHour = Number(course.begin.slice(0, 2))
   const endHour = Number(course.end.slice(0, 2))
-  const rowBegin = (beginHour - 6) * 2 + (course.begin.slice(3, 5) == '30' ? 1 : 0)   // Half hours are 1 row in length and the day starts at 6am + 1 if half hour
-  const rowEnd = (endHour - 6) * 2 + (course.end.slice(3, 5) == '30' ? 1 : 0)   // Half hours are 1 row in length and the day starts at 6am + if half hour
+  const rowBegin = (beginHour - 7) * 2 + (course.begin.slice(3, 5) == '30' ? 1 : 0)   // Half hours are 1 row in length and the day starts at 7am + 1 if half hour
+  const rowEnd = (endHour - 7) * 2 + (course.end.slice(3, 5) == '30' ? 1 : 0)   // Half hours are 1 row in length and the day starts at 7am + if half hour
   return (
     <div key={course.begin} className={clsx(
-      "Course w-full  text-gray-700 outline-2 outline-white relative bg-gradient-to-b p-2 font-marianne\
-      from-third-purple to-third-purple row-start-" + rowBegin + " row-end-" + rowEnd + " flex flex-row items-center")}
+      "Course w-full  text-gray-700 outline-2 outline-white relative font-marianne bg-third-purple\
+      row-start-" + rowBegin + " row-end-" + rowEnd + " flex flex-row items-center")}
       style={{
         gridRowStart: rowBegin, gridRowEnd: rowEnd,  // Because it doesnt work with tailwind
       }}>
       <CoursePlace place={course.place}/>
-      <div className="inline-block w-[80%]">
-        <div className="w-full flex items-start flex-col justify-center">
-          <h3 className="font-bold text-base drop-shadow-sm font-dinCondensed overflow-hidden whitespace-nowrap">
-            {course.name + "vfsdfdsfjsdlfkdjsfl"}
+      <div className="inline-block w-[80%] h-full bg-third-purple">
+        <div className="w-full h-full flex items-start flex-col justify-evenly">
+          <h3 className="font-normal text-base font-dinCondensed overflow-hidden whitespace-nowrap">
+            {course.name}
           </h3>
-          <h4 className="text-xs font-Charter">
+          <h4 className="text-[0.68rem] leading-3 font-Charter ">
             {course.teachers.join(', ')}
           </h4>
           {/* <h4 className="overflow-hidden text-[0.5rem] text-end"> {course.classes} </h4> */}
@@ -120,7 +119,7 @@ function CoursePlace({ place }) {
     <div className= { "inline-block h-full w-[20%] overflow-hidden text-start items-baseline font-extrabold \
     font-dinAlternate" }>
       <div className="flex flex-col justify-center items-start h-full w-full">
-        <h2 className={ "text-main-purple-dark "+textSize }>
+        <h2 className={ "text-main-purple-dark mx-auto "+textSize }>
           {place}
         </h2>
       </div>
@@ -130,8 +129,7 @@ function CoursePlace({ place }) {
 
 function DayContainer({ children }) {
   return (
-    <div className="DayContainer w-full h-full text-lg flex 
-    justify-center items-center">
+    <div className="DayContainer w-full h-full text-lg flex justify-center items-center">
       <div className='absolute top-0 w-72 h-[95%] z-0'>
         <div className='bg-gradient-to-r from-main-purple-light to-main-purple -z-[1] absolute left-0 top-0 bottom-0 right-0 rounded-xl'>
           <div className="DayAbsoluteContainer top-0 w-full h-full bg-white border-[1px] border-transparent box-border bg-clip-content overflow-hidden">
@@ -146,10 +144,10 @@ function DayContainer({ children }) {
 function DayContent({ courses, coursesHours }) {
   return (
     <div className="DayContent relative h-[89%] flex justify-start">
-      <div className="absolute inline-grid grid-rows-27 w-full h-full z-10 -top-[1.8518%]">
+      <div className="absolute inline-grid grid-rows-25 w-full h-full z-10 -top-[2%]">
         {coursesHours}
       </div>
-      <div className="inline-grid grid-rows-27 w-full h-full">
+      <div className="absolute inline-grid grid-rows-25 w-full h-full">
         {courses}
       </div>
     </div>
@@ -158,11 +156,8 @@ function DayContent({ courses, coursesHours }) {
 
 function DayName({ name, date }) {
   return (
-    <div className="DayName absolute flex 
-                  justify-center items-center flex-col text-center w-full 
-                  bottom-0 h-[0.1428]">
-      <h2 className="text-3xl font-light text-main-purple font-academyLET
-                    translate-y-3">
+    <div className="DayName absolute flex justify-center items-center flex-col text-center w-full bottom-0 h-[0.1428]">
+      <h2 className="text-3xl font-light text-main-purple font-academyLET translate-y-3">
         {name.toUpperCase()}
       </h2>
       <h4 className="text-xs font-normal mt-[0.1rem] text-gray-600 font-dinAlternate">
